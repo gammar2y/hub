ece3741c7ffb244b94f38091694224136f6a3118
gammar2y <156562743+gammar2y@users.noreply.github.com>
2024-05-01 16:45:34 -0600

    Update search.js

--- search.js
+++ search.js
@@ -1,4 +1,70 @@
 import React, {useContext, createContext} from 'react';
+const jwt = require('jsonwebtoken');
 const search = () => {
   const input = document.getElementbyId("user-search");
 }
+app.post("/register", (req,res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+
+  if (username && password) {
+    users.push({"username":username,"password":password});
+   return res.send("Account Created!")
+        )}};
+const doesExist = (username)=>{
+  let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+app.use(session({secret:"fingerpint"},resave=true,saveUninitialized=true));
+
+const authenticatedUser = (username,password)=>{
+  let validusers = users.filter((user)=>{
+     return (user.username === username && user.password === password)
+  });
+  if(validusers.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+app.post("/login", (req,res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+      return res.status(404).json({message: "Error logging in"});
+  }
+ if (authenticatedUser(username,password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+
+    req.session.authorization = {
+      accessToken,username
+  }
+  return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({message: "Invalid Login. Check username and password"});
+  }});
+app.use("/cards", function auth(req,res,next){
+   if(req.session.authorization) {
+       token = req.session.authorization['accessToken'];
+       jwt.verify(token, "access",(err,user)=>{
+           if(!err){
+               req.user = user;
+               next();
+           }
+           else{
+               return res.status(403).json({message: "User not authenticated"})
+           }
+        });
+    } else {
+        return res.status(403).json({message: "User not logged in"})
+    }
+});
