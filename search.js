b1f6cbc96757c734d61866b4211b75a84ebc381b
gammar2y <156562743+gammar2y@users.noreply.github.com>
2024-05-01 18:30:42 -0600

    Update search.js

--- search.js
+++ search.js
@@ -2,4 +2,14 @@ import React, {useContext, createContext} from 'react';
 const search = () => {
   const input = document.getElementbyId("user-search");
 }
-
+const result = cards.filter(checkCard);
+function checkCard (name) {
+  return item.startsWith (name);
+}
+  const filtered.Cards = {result.innerHTML};
+app.post("/search",(req,res)=>{
+        if (search.onclick = true)
+          then(function( input.push({"input":input}) ); 
+              return result (res.send(filtered.Cards))
+        
+        )}
